83ffd2d138ef2a4b84148258df6bac3b67c79f0e
Nguyen Xuan An <lanehacker7294@yahoo.com>
2019-12-11 17:27:41 +0700

    continue

--- src/Rabin_Karp.java
+++ src/Rabin_Karp.java
@@ -4,6 +4,8 @@ public class Rabin_Karp {
     Scanner scanner;
     String pattern;
     String[] arrText;
+    int hashPattern;
+    int[] hashText;
     int n;
 
     Rabin_Karp() {
@@ -16,9 +18,22 @@ public class Rabin_Karp {
         System.out.println("Input number of Text in array: ");
         n = scanner.nextInt();
         arrText = new String[n];
+        hashText = new int[n];
+
         for (int i = 0; i < n; i++) {
             System.out.println("Input " + i + 1 + " Text");
             arrText[i] = scanner.nextLine();
+        }
+    }
+
+    public void search() {
+        int step = 0;
+        for (int i = pattern.length() - 1; i >= 0; i--) {
+            hashPattern += ((int) pattern.charAt(i) * Math.pow(256, step)) % (Math.pow(2, 64) - 1);
+            for (int j = 0; j < n; j++) {
+                hashText[j]= (int) 
+            }
+            step += 1;
         }
     }
 }
